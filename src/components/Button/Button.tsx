f048d73a4931c5469b83d469a573ac6120a15c09
Shubham.Singh <shubham.singh@nw18.com>
2022-11-23 17:24:58 +0530

    2nd push

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,10 +2,12 @@ import React from "react";
 
 export interface ButtonProps {
   label: string;
+  className: string;
+  onClick: Function;
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button className={props.className} onClick={props.onClick()}>{props.label}</button>;
 };
 
 export default Button;
